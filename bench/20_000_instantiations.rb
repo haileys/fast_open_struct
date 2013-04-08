011840386ce04a52349ef9e6959e34eaf7e7a75a
Charlie Somerville <charlie@charliesomerville.com>
2013-04-08 23:39:17 +1000

    add benchmarks

---
+++ bench/20_000_instantiations.rb
@@ -0,0 +1,21 @@
+require "benchmark"
+include Benchmark
+
+require "fast_open_struct"
+require "ostruct"
+
+puts "20,000 instantiations:"
+
+bm 14 do |b|
+  b.report "OpenStruct" do
+    20_000.times do
+      OpenStruct.new a: 1, b: 2, c: 3
+    end
+  end
+
+  b.report "FastOpenStruct" do
+    20_000.times do
+      FastOpenStruct.new a: 1, b: 2, c: 3
+    end
+  end
+end
---
+++ bench/attribute_lookup.rb
@@ -0,0 +1,27 @@
+require "benchmark"
+include Benchmark
+
+require "fast_open_struct"
+require "ostruct"
+
+puts "Attribute lookups:"
+
+bm 14 do |b|
+  b.report "OpenStruct" do
+    os = OpenStruct.new a: 1, b: 2, c: 3
+    1_000_000.times do
+      os.a
+      os.b
+      os.c
+    end
+  end
+
+  b.report "FastOpenStruct" do
+    os = FastOpenStruct.new a: 1, b: 2, c: 3
+    1_000_000.times do
+      os.a
+      os.b
+      os.c
+    end
+  end
+end
